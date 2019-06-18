2f50420969879b322a34380466592c245ac1a4a3
mosheavidor1 <47886429+mosheavidor1@users.noreply.github.com>
2019-06-18 14:23:37 +0300

    Update UploadDriver.java
    g

--- src/test/java/MyPack/UploadDriver.java
+++ src/test/java/MyPack/UploadDriver.java
@@ -8,10 +8,15 @@ import org.openqa.selenium.WebDriver;
 
 public class UploadDriver {
 
+    public static final String upload1="webdriver.chrom.driver";
+    public static final String upload2= "C:\\Program Files\\chromedriver.exe";
+
 
         public static void FirefoxD(WebDriver driver){
 
-            WebDriverManager.firefoxdriver().setup();
+
+
+           // WebDriverManager.firefoxdriver().setup();
         }
 
 
