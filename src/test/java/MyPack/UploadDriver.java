0ab36605750ddbb1e5ff4a3293761ab50303d416
mosheavidor1 <47886429+mosheavidor1@users.noreply.github.com>
2019-06-18 15:00:59 +0300

    Update UploadDriver.java
    u

--- src/test/java/MyPack/UploadDriver.java
+++ src/test/java/MyPack/UploadDriver.java
@@ -1,9 +1,5 @@
 package MyPack;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 
 public class UploadDriver {
